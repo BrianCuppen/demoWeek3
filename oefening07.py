5bf51faf2944d3292f823c002ead6514432d5a9e
BrianCuppen <brian.cuppen@student.howest.be>
2021-10-05 09:49:31 +0200

    labo05/10

---
+++ oefening07.py
@@ -0,0 +1,17 @@
+
+#Vraag aan de gebruiker volgende 3 getallen op:
+
+#Deze functie geeft een string terug besetaande uit getallen
+#Die moeten afgeprint worden
+def geef_lijst_getallen():
+    resultaat = ""
+
+
+    return resultaat
+
+startwaarde = int(input(f"Geef de startwaarde op: >"))
+stapgroote = int(input(f"Geef een positieve stapgroote op: >"))
+aantal_getallen = int(input(f"Geef het aantal getallen op: >"))
+tekst = geef_lijst_getallen(startwaarde, stapgroote, aantal_getallen)
+print(tekst)
+
